e78fae98f589ad81e278dc9f9ca04cb36d8f54b6
Caroline <carolinepgabean@gmail.com>
2025-09-16 02:32:58 +0000

    Home Inertia React

---
+++ resources/js/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <div>
+            <Head title="Home" />
+            <h1>Welcome to Laravel + React + Inertia!</h1>
+        </div>
+    );
+}
